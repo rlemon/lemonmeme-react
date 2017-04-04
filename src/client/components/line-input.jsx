6a6f49eed8ecf0d6b5c2ed0da112341ab1a0d9fd
rlemon <rob.lemon@gmail.com>
2017-04-04 16:34:31 -0400

    placeholder for later

---
+++ src/client/components/line-input.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// TODO: this component
+/* provide an input and corresponding formatting controls for the line. pass up options and text with onChange emitter. */
+
+export default class LineInput extends Component {
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		return <div></div>;
+	}
+}
